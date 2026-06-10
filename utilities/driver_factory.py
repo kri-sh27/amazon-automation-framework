71b663fd6baefccb76379c9338f28bfa66d07923
kri-sh27 <hogalekrishna2000@gmail.com>
2026-06-11 01:48:11 +0530

    Integrated Lamdba test

--- utilities/driver_factory.py
+++ utilities/driver_factory.py
@@ -1,24 +1,75 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
 
 from utilities.config_reader import ConfigReader
 
+# # def get_driver():
+
+# #     options = webdriver.ChromeOptions()
+
+# #     options.add_argument("--start-maximized")
+
+# #     driver = webdriver.Chrome(
+# #         service=Service(
+# #             ChromeDriverManager().install()
+# #         ),
+# #         options=options
+# #     )
+
+# #     return driver
+
+
+
 # def get_driver():
 
-#     options = webdriver.ChromeOptions()
+#     execution = ConfigReader.get("execution")
 
-#     options.add_argument("--start-maximized")
+#     if execution.lower() == "cloud":
 
-#     driver = webdriver.Chrome(
-#         service=Service(
-#             ChromeDriverManager().install()
-#         ),
-#         options=options
-#     )
+#         username = ConfigReader.get("username")
+#         access_key = ConfigReader.get("access_key")
 
-#     return driver
+#         grid_url = (
+#             f"https://{username}:{access_key}"
+#             "@hub.lambdatest.com/wd/hub"
+#         )
 
+#         capabilities = {
+#             "browserName": "Chrome",
+#             "browserVersion": "latest",
+#             "LT:Options": {
+#                 "platformName": "Windows 11",
+#                 "project": "Amazon Assignment",
+#                 "build": "Build-1",
+#                 "name": "Amazon Parallel Test"
+#             }
+#         }
+
+#         driver = webdriver.Remote(
+#             command_executor=grid_url,
+#             options=webdriver.ChromeOptions()
+#         )
+
+#         driver.capabilities.update(capabilities)
+
+#         return driver
+
+#     else:
+
+#         options = webdriver.ChromeOptions()
+#         options.add_argument("--start-maximized")
+
+#         return webdriver.Chrome(
+#             service=Service(
+#                 ChromeDriverManager().install()
+#             ),
+#             options=options
+#         )
+
+
+import os
+from selenium import webdriver
 
 
 def get_driver():
@@ -27,42 +78,29 @@ def get_driver():
 
     if execution.lower() == "cloud":
 
-        username = ConfigReader.get("username")
-        access_key = ConfigReader.get("access_key")
+        username = os.getenv("LT_USERNAME")
+        access_key = os.getenv("LT_ACCESS_KEY")
 
-        grid_url = (
-            f"https://{username}:{access_key}"
-            "@hub.lambdatest.com/wd/hub"
-        )
+        print("LT_USERNAME:", username)
 
-        capabilities = {
-            "browserName": "Chrome",
-            "browserVersion": "latest",
-            "LT:Options": {
+        options = webdriver.ChromeOptions()
+
+        options.set_capability(
+            "LT:Options",
+            {
+                "username": username,
+                "accessKey": access_key,
                 "platformName": "Windows 11",
                 "project": "Amazon Assignment",
                 "build": "Build-1",
-                "name": "Amazon Parallel Test"
+                "name": "Parallel Execution",
+                "w3c": True
             }
-        }
+        )
 
         driver = webdriver.Remote(
-            command_executor=grid_url,
-            options=webdriver.ChromeOptions()
-        )
-
-        driver.capabilities.update(capabilities)
-
-        return driver
-
-    else:
-
-        options = webdriver.ChromeOptions()
-        options.add_argument("--start-maximized")
-
-        return webdriver.Chrome(
-            service=Service(
-                ChromeDriverManager().install()
-            ),
+            command_executor=f"https://{username}:{access_key}@hub.lambdatest.com/wd/hub",
             options=options
         )
+
+        return driver
